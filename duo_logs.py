9407afc9e172c3ac3be524d9b83a018762eb53e0
Jacob Backon <jbackon@babson.edu>
2022-10-25 11:18:43 -0400

    Initial commit

---
+++ duo_logs.py
@@ -0,0 +1,9 @@
+# create list of trash users from .csv file
+
+# import authentication log
+
+# create dictionary of user:[auth]
+
+# search through auth log and add user:[auth type] to dictionary
+
+# print out dictionary as .csv file
